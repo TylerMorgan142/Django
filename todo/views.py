4b87ba417fbb10a017094777c0a5852b294ed667
TylerMorgan142 <tylermorgan142@gmail.com>
2023-08-18 02:47:04 +0000

    Added functionality for adding items

--- todo/views.py
+++ todo/views.py
@@ -9,3 +9,12 @@ def get_todo_list(request):
     }
     return render(request, 'todo/todo_list.html', context)
 
+
+def add_item(request):
+    if request.method == "Post":
+        name = request.post.get("item_name")
+        done = "done" in request.Post
+        Item.objects.create(name=name, done=done)
+
+        return redirect("get_todo_list")
+    return render(request, 'todo/add_item.html')
